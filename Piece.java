306d807569a1c2d2cc53f67c34386bd701cff72b
pierreglt7 <giletpierre56@gmail.com>
2025-04-02 10:26:35 +0200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,14 +1,36 @@
 package projet;
 
 public class Piece {
-	public int type;
+	public String type;
 	public String emplacement;
 	
-	public Piece(int type,String emplacement) {
+	public Piece(String type,String emplacement) {
 		this.type=type;
 		this.emplacement=emplacement;
 		
-		if(type=)
+		if(type =="0x2656" || type =="0x265C") {  //Coup possible pour la tour
+			
+		}
+		
+		if(type =="0x2658" || type =="0x265E") {  //Coup possible pour le cavalier
+			
+		}
+		
+		if(type =="0x2657" || type =="0x265D") {  //Coup possible pour la fou
+			
+		}
+		
+		if(type =="0x2659" || type =="0x265F") {  //Coup possible pour le pion
+			
+		}
+		
+		if(type =="0x2655" || type =="0x265B") {  //Coup possible pour la reine
+			
+		}
+		
+		if(type =="0x2656" || type =="0x265A") {  //Coup possible pour le roi
+			
+		}
 	}
 	
 }
